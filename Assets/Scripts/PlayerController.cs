1921d1905c0ba3bd5b92f29277ffceb04edfd9b8
nural-khalafov <nural.khalafov@gmail.com>
2022-04-03 22:18:25 +0300

    Runner Project 2D
    - Added script with inverting gravity for player pressing double space button
    - Added player ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _direction;
+
+    private float _jumpScale = 20f;
+    private float _tapInterval = 0.35f;
+    private float _lastTapTime;
+
+
+    [SerializeField]
+    private bool _intertedGravity = false;
+
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            float timeSinceLastTap = Time.time - _lastTapTime;
+
+            if (timeSinceLastTap <= _tapInterval)
+            {
+                if (!_intertedGravity)
+                {
+                    _intertedGravity = true;
+                    _rigidbody.gravityScale = -5f;
+                }
+                else
+                {
+                    _intertedGravity = false;
+                    _rigidbody.gravityScale = 5f;
+                }
+            }
+            _lastTapTime = Time.time;
+
+            if (_intertedGravity)
+            {
+                _rigidbody.velocity = Vector2.down * _jumpScale;
+            }
+            else
+                _rigidbody.velocity = Vector2.up * _jumpScale;
+        }
+    }
+}
